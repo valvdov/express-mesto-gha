e9315c3b43b4f3855c724b915bd2427f9ead2b35
valvdov <valvdov@gmail.com>
2022-11-26 16:51:57 +0000

    fixes: sprint 13 delete unnecessary spaces

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-    User.findById(req.params.id)
+  User.findById(req.params.id)
     .then((user) => {
       if (user) res.send({ data: user });
       else res.status(404).send({ message: `Пользователь по указанному id(${req.params.id}) не найден` });
