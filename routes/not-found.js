901ee9b5848b8954fafb5751a25b0a3cd0880ce0
valvdov <valvdov@gmail.com>
2022-12-13 16:59:48 +0000

    fixes: sprint 14 pageNotFound test

--- routes/not-found.js
+++ routes/not-found.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const NotFoundError = require('../errors/NotFoundError');
+const { NotFoundError } = require('../errors/NotFoundError');
 
 router.all('*', () => {
   throw new NotFoundError('Страница не найдена');
